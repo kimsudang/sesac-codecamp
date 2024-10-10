b19650724fdf2ac65a6addf0e7b066f7f386e9f9
main <0ksuyeon0@gmail.com>
2024-10-10 11:38:29 +0900

    feat: 배너 이미지 페이지네이션 및 네비게이션 기능 추가

---
+++ frontend/next-homework/src/app/components/boards-list/banner/index.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Image from "next/image";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+
+const BoardListBannerComponent = () => {
+  return (
+    <div style={{ width: "100vw" }}>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        pagination={{
+          dynamicBullets: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Image
+            src="/images/banner01.jpeg"
+            alt="banner1"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: "100%", height: "512px", objectFit: "cover" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <Image
+            src="/images/banner02.jpeg"
+            alt="banner2"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: "100%", height: "512px", objectFit: "cover" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <Image
+            src="/images/banner03.jpeg"
+            alt="banner3"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: "100%", height: "512px", objectFit: "cover" }}
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default BoardListBannerComponent;
