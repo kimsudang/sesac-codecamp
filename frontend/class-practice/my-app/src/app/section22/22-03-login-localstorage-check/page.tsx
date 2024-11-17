aed237dd0f2e407e0c707fd0cf7567783ea34a1f
main <0ksuyeon0@gmail.com>
2024-11-17 11:23:51 +0900

    [241029] 수업 코드: 로그인 localstorage 저장 및 권한 분기

---
+++ frontend/class-practice/my-app/src/app/section22/22-03-login-localstorage-check/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useAccessTokenStore } from "@/commons/stores/22-01-access-token-store";
+import { gql, useMutation } from "@apollo/client";
+import { useRouter } from "next/navigation";
+import { ChangeEvent, useState } from "react";
+
+const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      accessToken
+    }
+  }
+`;
+
+export default function LoginPage() {
+  const router = useRouter();
+  const [loginUser] = useMutation(LOGIN_USER);
+  const { setAccessToken } = useAccessTokenStore();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChangeEmail = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const onChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const onClickLogin = async () => {
+    // 1. 로그인 뮤테이션 날려서 accessToken 받아오기
+    const result = await loginUser({
+      variables: {
+        email: email,
+        password: password,
+      },
+    });
+    const accessToken = result.data.loginUser.accessToken;
+    console.log(accessToken);
+
+    // 2. 받아온 accessToken을 globalState에 저장하기
+    if (accessToken === undefined) {
+      alert("로그인에 실패했습니다. 다시 시도해주세요.");
+    }
+    // accessToken 전달
+    setAccessToken(accessToken);
+    // key-value
+    // 이때 value에 객체는 들어가지 못함
+    localStorage.setItem("accessToken", accessToken);
+
+    // 3. 로그인 성공 페이지로 이동
+    router.push("/section22/22-03-login-localstorage-check-success");
+  };
+  return (
+    <>
+      이메일: <input type="text" onChange={onChangeEmail} />
+      <br />
+      비밀번호: <input type="password" onChange={onChangePassword} />
+      <br />
+      <button onClick={onClickLogin}>login</button>
+    </>
+  );
+}
